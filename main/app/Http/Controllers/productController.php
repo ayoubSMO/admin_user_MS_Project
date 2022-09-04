68d6d0d9ce2d1653a69c182c31f3778002e24ebf
ayoubSMO <84507906+ayoubSMO@users.noreply.github.com>
2022-09-04 14:31:08 +0100

    BackEnd Almost done !

--- main/app/Http/Controllers/productController.php
+++ main/app/Http/Controllers/productController.php
@@ -2,8 +2,13 @@
 
 namespace App\Http\Controllers;
 
+use App\Jobs\ProductLiked;
 use App\Models\Product;
+use App\Models\ProductUser;
+use Exception;
 use Illuminate\Http\Request;
+use Illuminate\Http\Response as HttpResponse;
+use Response;
 
 class productController extends Controller
 {
@@ -12,9 +17,26 @@ class productController extends Controller
     }
 
     public function like($id ,Request $request){
-        echo 'im here atbbi !!' ;
+        $response = \Http::get('http://docker.for.mac.localhost:8000/api/user');
+        $user = $response->json();
 
-        $response = \Http::get('http://docker.for.win.localhost:8000/api/user');
-        return $response->json();
+        try{
+        $productUser = ProductUser::create([
+            'user_id' => $user['id'],
+            'product_id' => $id
+        ]);
+
+        ProductLiked::dispatch($productUser->toArray());
+
+        return response([
+            'message' => 'success'
+        ]);
+
+    }catch(\Exception $exception){
+        return response(
+            [
+                'error' => 'You already likes this product !!'
+            ], HttpResponse::HTTP_BAD_REQUEST);
+    }
     }
 }
